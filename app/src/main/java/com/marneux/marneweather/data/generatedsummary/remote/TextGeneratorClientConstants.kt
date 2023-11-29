85632f6d5625c2b84d46fdd0f9cf04621140710c
Jorge Gimeno <jorgegimeno.f@gmail.com>
2023-11-29 10:24:43 +0100

    ui upgraded

--- app/src/main/java/com/marneux/marneweather/data/generatedsummary/remote/TextGeneratorClientConstants.kt
+++ app/src/main/java/com/marneux/marneweather/data/generatedsummary/remote/TextGeneratorClientConstants.kt
@@ -3,7 +3,7 @@ package com.marneux.marneweather.data.generatedsummary.remote
 object TextGeneratorClientConstants {
 
     const val BASE_URL = "https://api.openai.com/v1/chat/"
-    const val GPT_TOKEN = "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"
+    const val GPT_TOKEN = ""
 
     object Endpoints {
         const val CHAT_COMPLETION_END_POINT = "completions"
